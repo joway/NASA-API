6e533524dc70b65dd1b307123ecc936d995a51d0
JowayWang <joway.w@gmail.com>
2019-11-17 12:29:07 +0800

    add topics

--- generate.py
+++ generate.py
@@ -10,6 +10,28 @@ def write_file(path, content):
         f.write(content)
 
 
+def gen_topics():
+    data = [
+        {
+            'id': '3643', 'title': 'Mars Curiosity',
+            'imageUrl': 'images-assets.nasa.gov/image/PIA14760/PIA14760~orig.jpg',
+        },
+        {
+            'id': '3456', 'title': 'Space Station',
+            'imageUrl': 'https://images-assets.nasa.gov/image/9131518/9131518~large.jpg'
+        },
+        {
+            'id': '3451', 'title': 'Hubble Space Telescope',
+            'imageUrl': 'https://images-assets.nasa.gov/image/9015550/9015550~orig.jpg'
+        },
+        {
+            'id': '3631', 'title': 'Jono: Mission at Jupiter',
+            'imageUrl': 'https://images-assets.nasa.gov/image/PIA13087/PIA13087~orig.jpg'
+        },
+    ]
+    write_file(f'{BASE_DIR}/topics.json', json.dumps(data))
+
+
 def gen_today():
     today_rss_link = 'https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss'
     rss = feedparser.parse(today_rss_link)
@@ -31,4 +53,5 @@ def gen_today():
 
 
 if __name__ == '__main__':
+    gen_topics()
     gen_today()
